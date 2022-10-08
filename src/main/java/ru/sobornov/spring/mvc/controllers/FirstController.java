3c0a7f1264371beaaad72e9e17e49ebbb1060381
Vladimir Sobornov <thecodegod1@gmail.com>
2022-10-08 12:38:00 +0300

    added different get mappings

--- src/main/java/ru/sobornov/spring/mvc/controllers/FirstController.java
+++ src/main/java/ru/sobornov/spring/mvc/controllers/FirstController.java
@@ -3,6 +3,9 @@ package ru.sobornov.spring.mvc.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author : Sobornov Vladimir
@@ -14,12 +17,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage(){
+    public String helloPage(HttpServletRequest request){
+        String name = request.getParameter("name");
+        System.out.println("Name is: " + name);
         return "first/hello";
     }
 
     @GetMapping("/good-bye")
-    public String goodByePage(){
+    public String goodByePage(@RequestParam (value = "name", required = false) String name){
+        System.out.println("Name is " + name);
         return "first/good-bye";
     }
 }
